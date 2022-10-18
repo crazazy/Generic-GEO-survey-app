b49ff877d5c6881ffcd3247550a64c5125182f44
Erik Oosting <crazazy@tilde.cafe>
2022-10-18 11:18:11 +0200

    update location with a button
    Also doing some formatting

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 // turn the geolocation API into a Promise because callbacks are horrible
 const geo = (options) =>
-  new Promise((resolve, reject) =>
-    navigator.geolocation.getCurrentPosition(resolve, reject, options)
-  );
+      new Promise((resolve, reject) =>
+          navigator.geolocation.getCurrentPosition(resolve, reject, options)
+      );
 // create a map in the map div
 const map = L.map("map").setView([0, 0], 0);
+//updates the location on the map
+function updateLocation() {
+    //set options as per the source of
+    // https://paulojraposo.github.io/SomaliTreeSurvey/
+    // this function happens asynchronously
+    geo({
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    })
+    // only the coords are useful
+        .then((pos) => pos.coords)
+    // this is where all the basic map manipulation happens
+        .then((crd) => {
+            let pos = [crd.latitude.toFixed(5), crd.longitude.toFixed(5)];
+            // center the map based on where we are, with a zoom level of 13
+            map.setView(pos, 12);
+            // add a marker for a precise location
+            L.marker(pos).addTo(map);
+        })
+        .catch((err) => (document.body.innerHTML += err.toString()));
+}
+// get the location of the user upon loading the web page
+updateLocation();
+// mount the updating function to the button
+document.addEventListener("update", updateLocation);
 
-//set options as per the source of
-// https://paulojraposo.github.io/SomaliTreeSurvey/
-// this function happens asynchronously
-geo({
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-})
-  // only the coords are useful
-  .then((pos) => pos.coords)
-  // this is where all the basic map manipulation happens
-  .then((crd) => {
-    let pos = [crd.latitude.toFixed(5), crd.longitude.toFixed(5)];
-    // center the map based on where we are, with a zoom level of 13
-    map.setView(pos, 12);
-    // add a marker for a precise location
-    L.marker(pos).addTo(map);
-  })
-  .catch((err) => (document.body.innerHTML += err.toString()));
-
+// get a pretty image for the map from a tile server
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
+    attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
